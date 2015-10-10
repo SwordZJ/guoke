ecda621d6648405072c0c6af4dcf72e63ef159a3
mrRightZJ <707739952@qq.com>
2015-10-10 11:28:07 +0800

    初始化项目

---
+++ guoke/guoke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  guoke
+//
+//  Created by liuzhouji on 15/10/10.
+//  Copyright (c) 2015年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ guoke/guoke/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  guoke
+//
+//  Created by liuzhouji on 15/10/10.
+//  Copyright (c) 2015年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
